e485f076a7575d706ec3d83a4ffba8ddcb03dcdf
puskarthakur <puskar1303@gmail.com>
2023-06-07 19:21:59 +0530

    slider image

--- src/Booktest.js
+++ src/Booktest.js
@@ -7,16 +7,20 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Search from './Search';
+import Card from '@mui/material/Card';
 
 const StyledButton = styled(Button)(({ theme }) => ({
     backgroundColor: '#019a5d',
-      
+      margin: '8px'
   }));
   
   const StyledCardActions = styled(CardActions)(({ theme }) => ({
     display: 'flex',
     flexDirection:'column',
     justifyContent: 'space-between',
+  }));
+  const StyledTypography = styled(Typography)(({ theme }) => ({
+textAlign:'center'
   }));
   
 
@@ -25,11 +29,12 @@ const StyledButton = styled(Button)(({ theme }) => ({
 function Booktest(){
     return(
     
-        <div style={{textAlign: 'center'}}>
+        
+           <Card>
         <CardContent>
-          <Typography variant="h5" component="div">
+          <StyledTypography variant="h5" component="div">
             Book a Online Test
-          </Typography>
+          </StyledTypography>
      
         </CardContent>
         <StyledCardActions>
@@ -37,7 +42,8 @@ function Booktest(){
           <StyledButton size="small" variant='contained'>Packages</StyledButton>
           <StyledButton size="small" variant='contained'>MEDHA ADVANTAGE PLAN</StyledButton>
         </StyledCardActions>
-      </div>
+        </Card>
+      
 
   
     )
--- src/Home.js
+++ src/Home.js
@@ -2,13 +2,26 @@ import React from "react";
 import Header from './Header';
 import { Navbar } from "./Navbar";
 import Booktest from "./Booktest";
-
+import { Slideshow } from './Slideshow'
+import Grid from '@mui/material/Grid';
 function Home (){
     return(
         <div>
             <Navbar></Navbar>
             <Header></Header>
-            <Booktest></Booktest>
+            <Grid container spacing={2}>
+  <Grid item xs={8}>
+  <Slideshow></Slideshow>
+  </Grid>
+  <Grid item xs={2}>
+  <Booktest></Booktest>
+  </Grid>
+  <Grid item xs={2}>
+  </Grid>
+  </Grid>
+
+           
+           
         </div>
     )
 }
