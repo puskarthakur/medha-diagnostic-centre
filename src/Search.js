467edefd368fd075a82d38d7ad67cd06b6b3b70a
puskarthakur <puskar1303@gmail.com>
2023-06-17 11:05:32 +0530

    latest changes

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,13 @@
 import './styles/Search.css';
 import SearchIcon from '@mui/icons-material/Search'
+// import Button from '@mui/material/Button';
 export function Search() {
   return (
     <div className='search'>
       <div className='searchInputs'>
         <input type={'text'} placeholder="this is test..." />
         <div className='searchIcon'>
-          <SearchIcon/>
+         <button className='search-icon-btn'><SearchIcon/></button>
         </div>
       </div>
     </div>
